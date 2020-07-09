e778a06387a8f64964c651e7090420d25bbd0477
jfox55 <66087193+jfox55@users.noreply.github.com>
2020-07-09 15:21:37 -0400

    Update CompatabilityCalcular.py
    Editted code to bug fix

--- CompatabilityCalcular.py
+++ CompatabilityCalcular.py
@@ -7,33 +7,33 @@ user2 = input('User 2, What is your name?: ')
 user1responses = []
 user2responses = []
 questions = [
-    'Question 1. A) B) C) D)\n'
-    'Question 2. A) B) C) D)\n'
-    'Question 3. A) B) C) D)\n'
-    'Question 4. A) B) C) D)\n'
-    'Question 5. A) B) C) D)\n'
-    'Question 6. A) B) C) D)\n'
-    'Question 7. A) B) C) D)\n'
-    'Question 8. A) B) C) D)\n'
-    'Question 9. A) B) C) D)\n'
-    'Question 10. A) B) C) D)\n'
+    '\nQuestion 1. A) B) C) D)',
+    'Question 2. A) B) C) D)',
+    'Question 3. A) B) C) D)',
+    'Question 4. A) B) C) D)',
+    'Question 5. A) B) C) D)',
+    'Question 6. A) B) C) D)',
+    'Question 7. A) B) C) D)',
+    'Question 8. A) B) C) D)',
+    'Question 9. A) B) C) D)',
+    'Question 10. A) B) C) D)'
 ]
-def questionnaire1():
+def questionaire1():
     questions1 = questions
-    for question in questions1:
-        asking = questions1.pop()
-        print(asking)
-        user1responses.append = input('What is your answer?:')
-questionnaire1()
+    while questions1:
+        print(questions1.pop())
+        user1responses.append(input('What is your answer?:'))
+questionaire1()
+print(user1responses)
 
 #Repeat for second person
-def questionnaire2():
+def questionaire2():
     questions2 = questions
-    while question in questions2:
-        asking = questions2.pop()
-        print(asking)
-        user2responses.append = input()
-questionnaire2()
+    while questions2:
+        print(questions2.pop())
+        user2responses.append(input('What is your answer?:'))
+questionaire2()
+print(user2responses)
 
 #Compare repositories
 while True:
